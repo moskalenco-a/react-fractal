3fdb26741ccec3582b6125caa0852c557511de36
moskalenco_a <moskalenco.a@gmail.com>
2022-11-14 21:22:51 +0200

    Add comments

--- src/DrawMinkowskiFractal.js
+++ src/DrawMinkowskiFractal.js
@@ -1,12 +1,19 @@
 import { sum, length, rotated, scaled } from './vecUtils';
 
+// ctx - графічний контекст
+// n - кількість ітерацій
+// first, second - початкова і кінцева точка
+// vec - вектор напрямку
+// для горизонтальної прямої злів направо це (1; 0)
 const drawMinkowskiFractalImpl = (ctx, n, first, second, vec = { x: 1, y: 0 }) => {
   if (n === 0)
     return;
   if (second.y > first.y) vec = { x: 0, y: 1 };
   if (second.y < first.y) vec = { x: 0, y: -1 };
+  // якщо n дорівнює 1
   if (n === 1) {
-    ctx.beginPath();
+    // просто малюємо
+    // відрізок first - second
     ctx.beginPath();
     ctx.moveTo(first.x, first.y);
     ctx.lineTo(second.x, second.y);
@@ -14,43 +21,83 @@ const drawMinkowskiFractalImpl = (ctx, n, first, second, vec = { x: 1, y: 0 }) =
     // ctx.fill();
     return;
   }
+  // кут в 90 градусів
   const d90 = Math.PI / 2;
+  // чверть довжини відрізка
   const len = length(first, second) / 4;
+  // будь-який відрізок AB
+  // перетворюється на ламану з 8 частин
+  // довжина кожної з 8 частинин ламаної = чверть довжини AB(тобто len)
+
+  // малюємо CD
+  // вектор напряму такий же
   let newVec = vec;
+  // перша точка, C
   let newFirst = first;
+  // друга точка, D
+  // додаємо до першої точки
+  // вектор розтянутий до довжини len(довжина вектора CD)
+  // (nevVec дорівнює vec, а ми будемо в функцію передавати
+  //  vec завжди нормалізованим = тобто довжина вже дорівнює 1)
   let newSecond = sum(first, scaled(newVec, len));
+  // для CD рекурсивно малюєто таку криву
   drawMinkowskiFractalImpl(ctx, n-1, newFirst, newSecond, newVec);
 
+  // повертаємо вектор на 90 градусів
+  // щоб отримати вектор DE
   newVec = rotated(vec, -d90);
+  // початкова точка дорівнює
+  // кінцевій від попереднього відрізку
   newFirst = newSecond;
+  // точка E = D + вектор DE
   newSecond = sum(newFirst, scaled(newVec, len));
+  // для DE рекурсивно малюємо таку саму криву
   drawMinkowskiFractalImpl(ctx, n-1, newFirst, newSecond, newVec);
 
+  // малюємо EF
+  // вектор такий же як CD
   newVec = vec;
+  // початкова точка дорівнює
+  // кінцевій від попереднього відрізку
   newFirst = newSecond;
+  // кінцева точка = початкова + вектор
   newSecond = sum(newFirst, scaled(newVec, len));
+  // малюємо рекурсивно EF
   drawMinkowskiFractalImpl(ctx, n-1, newFirst, newSecond, newVec);
 
+  // повертаємо вектор на 90
+  // щоб отримати з вектору EF(CD)
+  // вектор FG
   newVec = rotated(vec, d90);
+  // малюємо рекурсивно FG
   newFirst = newSecond;
   newSecond = sum(newFirst, scaled(newVec, len));
   drawMinkowskiFractalImpl(ctx, n-1, newFirst, newSecond, newVec);
 
+  // з таким самим вектором
+  // малюємо GH
   // newVec = rotated(vec, d90);
   newFirst = newSecond;
   newSecond = sum(newFirst, scaled(newVec, len));
   drawMinkowskiFractalImpl(ctx, n-1, newFirst, newSecond, newVec);
 
+  // малюємо HI
+  // вектор такий же(як у CD, EF)
   newVec = vec;
   newFirst = newSecond;
   newSecond = sum(newFirst, scaled(newVec, len));
   drawMinkowskiFractalImpl(ctx, n-1, newFirst, newSecond, newVec);
 
+  // малюємо IJ
+  // вектор як у DE
   newVec = rotated(vec, -d90);
   newFirst = newSecond;
   newSecond = sum(newFirst, scaled(newVec, len));
   drawMinkowskiFractalImpl(ctx, n-1, newFirst, newSecond, newVec);
 
+  // малюємо JK
+  // вектор напряму такий же
+  // як у CD
   newVec = vec;
   newFirst = newSecond;
   newSecond = sum(newFirst, scaled(newVec, len));
@@ -58,10 +105,12 @@ const drawMinkowskiFractalImpl = (ctx, n, first, second, vec = { x: 1, y: 0 }) =
 };
 const Point = (x, y) => ({ x, y });
 
+// у цій функції
+// просто визначаємо вершини квадрату
+// і для кожної сторони малюємо одну криву рекурсивно
 export const drawMinkowskiFractal = (ctx, initialSize = 50, levels = 3, cx = 250, cy = 350) => {
   const first = Point(cx, cy);
   const second = Point(cx + initialSize, cy);
-  // drawMinkowskiFractalImpl(ctx, levels, first, second);
   drawMinkowskiFractalImpl(ctx, levels, first, second);
   drawMinkowskiFractalImpl(ctx, levels, first, Point(cx, cy + initialSize));
   drawMinkowskiFractalImpl(ctx, levels, Point(cx, cy + initialSize), Point(cx + initialSize, cy + initialSize));
--- src/DrawPythagorasTree.js
+++ src/DrawPythagorasTree.js
@@ -2,38 +2,86 @@ import { Point, sum, diff,
          normalize, scale, rotate
        } from './vecUtils';
 
+// малювання прямокутника
+// ctx графічний контекст
+// a, b, c, d вершини прямокутника
 const drawRect = (ctx, a, b, c, d) => {
+  // початок малювання нової лінії
   ctx.beginPath();
+  // переходимо у вершину A
   ctx.moveTo(a.x, a.y);
+  // з'єднуємо відрізком з B
   ctx.lineTo(b.x, b.y);
+  // з'єднуємо відрізком B з C
   ctx.lineTo(c.x, c.y);
+  // з'єднуємо відрізком C з D
   ctx.lineTo(d.x, d.y);
+  // з'єднуємо відрізком D з A
   ctx.lineTo(a.x, a.y);
+  // малюємо отриманий контур
   ctx.stroke();
+  // заповнюємо внутрішню область
   ctx.fill();
 };
 
+// малювання дерева, n кількість ітерацій
+// A, B, C, D вершини квадрату
 const drawPythagorasTreeImpl = (ctx, n, a, b, c, d) => {
+  // виходимо якщо n
+  // дорівнює нулю
   if (n === 0)
     return;
 
+  // кут EAD
   const alpha = Math.PI / 6;         // 30
+  // кут EDA, 90 градусів мінус інший гострий кут
   const  beta = Math.PI / 2 - alpha; // 60
-
+  // довжина сторони квадрату за теоремою Піфагора
+  // (тобто відстань між B, C)
   const side = Math.hypot(b.x - c.x, b.y - c.y);
+  // довжина сторони AE малого квадрату зліва
+  // = гіпотенуза AD * синус кута навпорти AE(кут EDA)
   const leftSide = side * Math.sin(beta);
+  // знаходимо вектор AD
+  // (від кінцевої точки віднімаємо початкову)
   let vec = diff(d, a);
+  // нормалізуємо = робимо так,
+  // щоб довжина вектора дорівнювала 1
+  // щоб потім розтянути вектор на потрібну довжну
   normalize(vec);
+  // повертаємо вектор на кут EAD
+  // так, щоб напрям вектора зпівпав зі стороною AE
   rotate(vec, -alpha);
+  // тепер розтягуємо вектор
+  // щоб його довжина дорівнювала довжині
+  // сторони AE
   scale(vec, leftSide);
+  // визначаємо вершини нового квадрату
+  // тобто точки F, A, E, G
+  // нова вершина B це просто A
   let newB = a;
+  // нова вершина C, це точка E
+  // додаємо до точки A(newB) вектор AE(vec) щоб отримати
+  // точку E(newC)
   let newC = sum(newB, vec);
+  // повертаємо вектор на 90 градусів
+  // щоб отримати з вектора AE
+  // вектор AF
   rotate(vec, -Math.PI / 2);
+  // нова вершина A це точка F
+  // F це A додати вектор AF(vec)
   let newA = sum(newB, vec);
+  // нова вершина D точка G
+  // ії можна отримати
+  // якщо той самий вектор AF(рівний вектору EG)
+  // додати до точки E(newC)
   let newD = sum(newC, vec);
   drawRect(ctx, a, b, c, d);
+  // рекурсивно малюємо фрактал
+  // для лівого квадрату
   drawPythagorasTreeImpl(ctx, n - 1, newA, newB, newC, newD);
 
+  // те ж саме для правого квадрату
   const rightSide = side * Math.sin(alpha);
   vec = diff(a, d);
   normalize(vec);
